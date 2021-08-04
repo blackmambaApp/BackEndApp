c76f7e499929a141aa07f50524bd0aed31fbc725
Amine <amichachi@alum.us.es>
2021-08-04 20:55:09 +0200

    Dependecia Excel4Node eliminada

--- server/app.js
+++ server/app.js
@@ -66,7 +66,7 @@ app.get('/', (req,res) => {
 //Starting the Server
 
 
-const port = app.get('port');
+const port =  process.env.PORT || 3000;
 const host = process.env.HOST || '0.0.0.0';
 app.listen(port,host, () => {
     console.log(`Server Working at http://${host}:${port}`)
